c447a924759072efa87c505679026b5c3c469354
yibeichan <yibeichan@gmail.com>
2024-01-11 15:39:02 +0000

    Deploying to gh-pages from @ ReproNim/eab-demo@0e0120b311effdc3e8372604deffe0888d1cc928 🚀

--- ui-changes/src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/repronim/eab-demo/main/eab_demo/eab_demo_schema',
-  banner: 'a demo for eab meeting',
-  startButton: 'Join',
-  assetsPublicPath: '/eab-demo/',
-  backendServer: null
-};
