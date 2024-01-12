dfd7cacba7004a8fc2e4dcdd70555d5dbf0556c4
Yibei Chen <yibeichan@gmail.com>
2024-01-12 09:01:44 -0500

    test "Start"

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   /* eslint-disable */
   githubSrc: 'https://raw.githubusercontent.com/repronim/eab-demo/main/eab_demo/eab_demo_schema',
   banner: 'a demo for eab meeting',
-  startButton: 'Join',
+  startButton: 'Start',
   assetsPublicPath: '/eab-demo/',
   backendServer: null
 };
